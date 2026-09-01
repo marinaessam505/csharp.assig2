9e8bd3ea8cce0bed5068ceecccb3b35edd0dbbbe
MARINA ESSAM <marinaessam505@gmail.com>
2026-09-02 01:05:27 +0300

    Q2

--- assigment2/assigment2/Program.cs
+++ assigment2/assigment2/Program.cs
@@ -1,21 +1,58 @@
 ﻿namespace assigment2
 {
 
-    #region Q1
+    #region Question 1
+
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+
+
+    //        string Text = "464";
+    //        int pages = Convert.ToInt32(Text);
+
+    //        Console.WriteLine(pages);
+
+
+    //    }
+    //}
+    #endregion
+
+
+    #region Question 2
 
     class Program
     {
         static void Main()
         {
-            
+            //PART ONE MN ALSO2AL
+            string yearText = "2023";
+            int year = int.Parse(yearText);
 
-            string Text = "464";
-            int pages = Convert.ToInt32(Text);
+            Console.WriteLine(year);
 
-            Console.WriteLine(pages);
+             // PART TWO
 
-            
+            string badText = "abc";
+            int number;
+
+            if (int.TryParse(badText, out number))
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Invalid number");
+            }
         }
     }
+
     #endregion
+
+
+
+
+
+
 }
